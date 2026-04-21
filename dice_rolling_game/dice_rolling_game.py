c9326f8d426f1a7f8aa28ae813d34c8cb0e2312f
Tashmia Anwar <tashmiaanwar@Tashmias-MBP.lan>
2026-04-21 10:50:20 -0500

    New feature to keep track of the number of rounds played

--- dice_rolling_game/dice_rolling_game.py
+++ dice_rolling_game/dice_rolling_game.py
@@ -1,10 +1,15 @@
 import random
 
+# Keeps track of the number of rounds played
+i = 0
+
 # Rolls two dice and displays the result
 def game():
+        global i
         die1 = random.randint(1,6)
         die2 = random.randint(1,6)
-        print(f"({die1}, {die2})")
+        i += 1
+        print(f"Round {i}: ({die1}, {die2})")
 
 # Plays the game if user says 'y', terminates the game if user says 'n', and returns error message for any other input
 while True:
