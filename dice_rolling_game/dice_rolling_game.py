09073339876a4ad21a8df283a2fcac45cdd7be31
Tashmia Anwar <tashmiaanwar@Tashmias-MBP.lan>
2026-04-21 10:56:30 -0500

    Cleaned up formatting

--- dice_rolling_game/dice_rolling_game.py
+++ dice_rolling_game/dice_rolling_game.py
@@ -26,8 +26,6 @@ def game():
     i += 1
     print(f"Round {i}: {rolls}")
     
-
-
 # Plays the game if user says 'y', terminates the game if user says 'n', and returns error message for any other input
 while True:
     answer = input("Roll the dice? (y/n): ").lower()
